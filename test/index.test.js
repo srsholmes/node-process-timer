030eaa30765d16eb03271c803257bcfd07e9dffd
srsholmes <srsholmes@gmail.com>
2017-04-27 10:29:56 +0100

    jest testing because im a cool kid

---
+++ test/index.test.js
@@ -0,0 +1,40 @@
+import sinon from 'sinon';
+import {
+  curry,
+  averageTime,
+  timer,
+  sum,
+  secondsToNano,
+  nanoToMilli,
+  nanoToSec,
+  milliToSec,
+} from '../src';
+
+test('curry', () => {
+  const add = curry((a, b) => a + b);
+  const addThenMultiply = curry((a, b, c) => (a + b) * c);
+  expect(add(1)(2)).toBe(add(1, 2));
+  expect(addThenMultiply(1, 2, 3)).toBe(9);
+});
+
+test('node-process-timer --time utils', () => {
+  expect(secondsToNano(1000)).toBe(1e+12);
+  expect(nanoToMilli(1000)).toBe(0.001);
+  expect(nanoToSec(1000)).toBe(1e-6);
+  expect(milliToSec(1000)).toBe(1);
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('node-process-timer', () => {
+  const time = timer(sum, [ 1, 2 ]);
+  const average = averageTime(sum, [ 1, 2 ], 10000);
+  const spy = sinon.spy();
+  expect(typeof timer(sum)).toBe('function');
+  expect(typeof averageTime(sum)).toBe('function');
+  expect(typeof time).toBe('number');
+  expect(typeof average).toBe('number');
+  expect(time > average).toBe(true);
+
+  timer(spy, ['hello']);
+  //t.deepEquals(spy.calledWith('hello'), true, 'should call the spy with the correct args')
+});
